d7424fb672d9e5c33ba66c8458d20244ace28a62
날자 <clipman@naver.com>
2023-02-15 13:41:39 +0900

    Update install_scripts.sh

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -6,19 +6,18 @@ wget -O /etc/default/moonraker https://raw.githubusercontent.com/clipman/klipper
 wget -O /etc/init.d/moonraker https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_init.d_moonraker
 wget -O /etc/default/socat https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_default_socat
 wget -O /etc/init.d/socat https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_init.d_socat
-wget -O /usr/local/bin/xterm https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/usr_local_bin_xterm
-wget -O /root/xterm.sh https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/xterm.sh
+wget -O /etc/default/xterm https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_default_xterm
+wget -O /etc/init.d/xterm https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_init.d_xterm
 
 chmod +x /etc/init.d/klipper
 chmod +x /etc/init.d/moonraker
 chmod +x /etc/init.d/socat
-chmod +x /usr/local/bin/xterm
-chmod +x /root/xterm.sh
+chmod +x /etc/init.d/xterm
 
 update-rc.d klipper defaults
 update-rc.d moonraker defaults
 update-rc.d socat defaults
+update-rc.d xterm defaults
 
 nano /etc/default/socat
-nano /usr/local/bin/xterm
-nano /root/xterm.sh
+nano /etc/default/xterm
