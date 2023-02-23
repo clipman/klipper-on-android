389cf55c5ee69dcd030df6470473ed2db512b704
날자 <clipman@naver.com>
2023-02-23 23:18:45 +0900

    Update install_scripts.sh

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -4,6 +4,8 @@ wget -O /etc/default/klipper https://raw.githubusercontent.com/clipman/klipper-o
 wget -O /etc/init.d/klipper https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_init.d_klipper
 wget -O /etc/default/moonraker https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_default_moonraker
 wget -O /etc/init.d/moonraker https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_init.d_moonraker
+wget -O /etc/default/crowsnest https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_default_crowsnest
+wget -O /etc/init.d/crowsnest https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_init.d_crowsnest
 wget -O /etc/default/socat https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_default_socat
 wget -O /etc/init.d/socat https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_init.d_socat
 wget -O /etc/default/xterm https://raw.githubusercontent.com/clipman/klipper-on-android/main/scripts/etc_default_xterm
@@ -11,11 +13,13 @@ wget -O /etc/init.d/xterm https://raw.githubusercontent.com/clipman/klipper-on-a
 
 chmod +x /etc/init.d/klipper
 chmod +x /etc/init.d/moonraker
+chmod +x /etc/init.d/crowsnest
 chmod +x /etc/init.d/socat
 chmod +x /etc/init.d/xterm
 
 update-rc.d klipper defaults
 update-rc.d moonraker defaults
+update-rc.d crowsnest defaults
 update-rc.d socat defaults
 update-rc.d xterm defaults
 
